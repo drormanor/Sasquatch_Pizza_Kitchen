0e47d04d039d91627cfd616550b807bc37fffa4b
Dror Manor <drormanor@student.hartnell.edu>
2018-05-15 15:38:02 -0700

    Dror - Worked on order class and header

--- Code/2B Final Project/include/Order.h
+++ Code/2B Final Project/include/Order.h
@@ -1,113 +1,168 @@
 #include "Node.h"
 #include "Pizza.h"
 #include <cstdlib>
+#include <time.h>
+#include <string>
 
 using namespace std;
 class Order
 {
-    public:
-        typedef Node<Pizza>::value_type data_type;
-        typedef size_t size_type;
-        // Constructor creates an empty list
-        // ******************************************************************************************
-        Order();
-        // Summary: Constructor creates a linked list object with count set at 0.
-        // ******************************************************************************************
-        Order(const Order & list2bcopy);
-        // Summary:  copy constructor creates a linked list object with count set at 0
-        // and copy all of list2bcopy into it.
-        // ******************************************************************************************
-        
-        // Accessor functions for the list's head_ptr
-        // ******************************************************************************************
-        Node<Pizza>* getHeadPtr();
-        // Summary: Returns a pointer to the head Node of the list.
-        // Preconditions: The list has been initialized.
-        // Postconditions: A pointer to the head has been returned.
-        // ******************************************************************************************
-        // ******************************************************************************************
-        const Node<Pizza>* getHeadPtr()const;
-        // Summary: Returns a pointer to the head Node of the list.
-        // Preconditions: The list has been initialized.
-        // Postconditions: A pointer to the head has been returned.
-        // ******************************************************************************************
-        //
-        // Accessor functions for the list's tail_ptr
-        // ******************************************************************************************
-        Node<Pizza>* getTailPtr();
-        const Node<Pizza>* getTailPtr()const;
-        // Summary: Returns a pointer to the end Node of the list.
-        // Preconditions: The list has been initialized.
-        // Postconditions: A pointer to the end of the list has been returned.
-        // ******************************************************************************************
-        // ******************************************************************************************
-        size_type list_length() const;
-        // Summary: This function returns the number of Nodes in the list.
-        // Preconditions:  A list exists
-        // Postconditions: Returns 0 if list is empty, otherwise it returns the number of items  in the list, i.e. count
-        // ******************************************************************************************
-        // ******************************************************************************************
-        bool isEmpty() const;
-        // Summary: This function returns true if the list is empty.
-        // Preconditions:  A list object exists
-        // Postconditions: Returns true if the list contains no Nodes and false if the list contains at least one Node.
-        // ******************************************************************************************
-        // ******************************************************************************************
-        void head_insert(const Node<Pizza>::value_type& value);
-        // Summary: This function inserts a new Node at the beginning of the list.
-        // Preconditions:  A head_ptr exists and a new value has been passed in.
-        // Postconditions: A new Node has been added to the beginning of the list with the value in the data_field at the head of the list.
-        // ******************************************************************************************
-        // ******************************************************************************************
-        void list_insert(Node<Pizza>* prev_ptr, const Node<Pizza>::value_type& value);
-        // Summary: This function inserts a new Node with value in the data field after the prev_ptr Node.
-        // Preconditions: prev_ptr is a pointer to the Node that is before the the spot where we want to insert a new Node with data equal to value.
-        // Postconditions: The list with one more Node that contains the value passed into the function.
-        // ******************************************************************************************
-        // ******************************************************************************************
+  public:
+    typedef Node<Pizza>::value_type data_type;
+    typedef size_t size_type;
+    // Constructor creates an empty list
+    // ******************************************************************************************
+    Order();
+    // Summary: Constructor creates a linked list object with count set at 0.
+    // ******************************************************************************************
+    Order(const Order &list2bcopy);
+    // Summary:  copy constructor creates a linked list object with count set at 0
+    // and copy all of list2bcopy into it.
+    // ******************************************************************************************
 
-        void head_remove();
-        // Summary: This function removes the Node at the beginning of the list.
-        // Preconditions: There is a list with head_ptr pointing to the beginning
-        // of the list.
-        // Postconditions: We have a list with the previous head Node of the list
-        // removed.
-        // *********************************************************************
-        // *********************************************************************
-        void list_remove(Node<Pizza>* prev_ptr);
-        // Summary: This function removes the Node after the prev_ptr Node.
-        // Preconditions: prev_ptr is a Node pointer that points to the Node
-        // before the Node we want to delete.
-        // Postconditions: The list has had one Node removed that use to be after
-        // the Node pointed to by prev_ptr
-        // *********************************************************************
-        // *********************************************************************
-        void clear_list();
-        // Summary: This function empties the list.
-        // Preconditions: There is a current list.
-        // Postconditions: list has been cleared of Nodes. head_ptr now is NULL.
-        // *********************************************************************
-        // *********************************************************************
-        void operator= (const Order& source_list);
-        //  Summary: will clear list and copy source list nodes into list.
-        // Preconditions: source list must not be empty.
-        // postconditions: list will be be populated with copies of source 
-        // list nodes
+    void set_phone(string phone);
+    // Summary:  setter for the customer phone
+    // ******************************************************************************************
 
-        ~Order();
-        // Summary: The destructor empties the list.
-        // *********************************************************************
-        
+    void set_name(string name);
+    // Summary:  setter for the customer name
+    // ******************************************************************************************
+    void set_cookTime(int cookTime);
+    // Summary:  setter for the order cook time
+    // ******************************************************************************************
 
-        private:
-             Node<Pizza>* head_ptr; // Points to front of list
-             Node<Pizza>* tail_ptr; // Points to end of list
-             size_type node_count;   // holds the number of Nodes in the list
- };
+    string get_phone(string phone) const;
+    // Summary:  getter for the customer phone
+    // ******************************************************************************************
 
-        // Non Member Functions
-        // *********************************************************************
-        void list_copy(const Order& source_list, Order& destination_list );
-        //  summary: will copy all nodes from source list to destination list
-        //  in the same order
-        // *********************************************************************
+    string get_name(string name) const;
+    // Summary:  getter for the customer name
+    // ******************************************************************************************
+
+    int get_cookTime() const;
+    // Summary:  getter for the order cook time
+    // ******************************************************************************************
+    time_t get_start_time()  const{ return order_time; };
+    // Summary: a timestamp will be returned
+    // *********************************************************************
+
+  
+    Node<Pizza> *getHeadPtr();
+    // Summary: Returns a pointer to the head Node of the list.
+    // Preconditions: The list has been initialized.
+    // Postconditions: A pointer to the head has been returned.
+    // ******************************************************************************************
+    // ******************************************************************************************
+    const Node<Pizza> *getHeadPtr() const;
+    // Summary: Returns a pointer to the head Node of the list.
+    // Preconditions: The list has been initialized.
+    // Postconditions: A pointer to the head has been returned.
+    // ******************************************************************************************
+
+    Node<Pizza> *getTailPtr();
+    // Summary: Returns a pointer to the end Node of the list.
+    // Preconditions: The list has been initialized.
+    // Postconditions: A pointer to the end has been returned.
+    // ******************************************************************************************
+
+    const Node<Pizza> *getTailPtr() const;
+    // Summary: Returns a pointer to the end Node of the list.
+    // Preconditions: The list has been initialized.
+    // Postconditions: A pointer to the end of the list has been returned.
+    // ******************************************************************************************
+    // ******************************************************************************************
+    size_type list_length() const;
+    // Summary: This function returns the number of Nodes in the list.
+    // Preconditions:  A list exists
+    // Postconditions: Returns 0 if list is empty, otherwise it returns the number of items  in the list, i.e. count
+    // ******************************************************************************************
+    // ******************************************************************************************
+    bool isEmpty() const;
+    // Summary: This function returns true if the list is empty.
+    // Preconditions:  A list object exists
+    // Postconditions: Returns true if the list contains no Nodes and false if the list contains at least one Node.
+    // ******************************************************************************************
+    // ******************************************************************************************
+    void head_insert(const Node<Pizza>::value_type &value);
+    // Summary: This function inserts a new Node at the beginning of the list.
+    // Preconditions:  A head_ptr exists and a new value has been passed in.
+    // Postconditions: A new Node has been added to the beginning of the list with the value in the data_field at the head of the list.
+    // ******************************************************************************************
+    // ******************************************************************************************
+    void list_insert(Node<Pizza> *prev_ptr, const Node<Pizza>::value_type &value);
+    // Summary: This function inserts a new Node with value in the data field after the prev_ptr Node.
+    // Preconditions: prev_ptr is a pointer to the Node that is before the the spot where we want to insert a new Node with data equal to value.
+    // Postconditions: The list with one more Node that contains the value passed into the function.
+    // ******************************************************************************************
+    // ******************************************************************************************
+
+    void head_remove();
+    // Summary: This function removes the Node at the beginning of the list.
+    // Preconditions: There is a list with head_ptr pointing to the beginning
+    // of the list.
+    // Postconditions: We have a list with the previous head Node of the list
+    // removed.
+    // *********************************************************************
+    // *********************************************************************
+    void list_remove(Node<Pizza> *prev_ptr);
+    // Summary: This function removes the Node after the prev_ptr Node.
+    // Preconditions: prev_ptr is a Node pointer that points to the Node
+    // before the Node we want to delete.
+    // Postconditions: The list has had one Node removed that use to be after
+    // the Node pointed to by prev_ptr
+    // *********************************************************************
+    // *********************************************************************
+    void clear_list();
+    // Summary: This function empties the list.
+    // Preconditions: There is a current list.
+    // Postconditions: list has been cleared of Nodes. head_ptr now is NULL.
+    // *********************************************************************
+    // *********************************************************************
+
+    void take_order();
+    // Summary: This function will take all info from user and store in the variables
+    // also, will create the pizza nodes and store them in a lincked list.
+    //Precondition: a Order object instantiated.
+    // Postconditions: an order object is populated with pizza nodes and customer info.
+
+    void print_order();
+    // Summary: this function will print summary of all object info.
+
+    bool is_ready();
+    // Summary: this function will check if an order is ready.
+    // Preconditons: object has been created and pizza list is not empty
+    // Postconditions: a bool is returned after comparing the order_time to the cook_time
+
+    bool is_delivered();
+    // Summary: this function will check if an order was delivered.
+    // Preconditons: object has been created and pizza list is not empty
+    // Postconditions: a bool is returned after comparing the order_time to the cook_time+deliverry_time;
+
+    ~Order();
+    // Summary: The destructor empties the list.
+    // *********************************************************************
+
+  private:
+    int cook_time;
+    int deliverry_time = 30;
+    string customer_name;
+    string customer_phone;
+    Node<Pizza> *head_ptr; // Points to front of list
+    Node<Pizza> *tail_ptr; // Points to end of list
+    size_type pizza_count; // holds the number of Nodes in the list
+    time_t order_time;     //store the time when the order was placed
+
+    void set_start_time() { order_time = time(NULL); };
+    // Summary: a timestamp will be created and stored
+    // *********************************************************************
+
+    void add_pizza();
+
+};
+
+// Non Member Functions
+// *********************************************************************
+void list_copy(const Order &source_list, Order &destination_list);
+//  summary: will copy all nodes from source list to destination list
+//  in the same order
+// *********************************************************************
--- Code/2B Final Project/src/Order.cpp
+++ Code/2B Final Project/src/Order.cpp
@@ -1,52 +1,78 @@
 #include "./include/Order.h"
 //#include "Order.h"
 
-
-#include<cassert>
-#include<iostream>
+#include <cassert>
+#include <iostream>
 
 Order::Order()
 {
-    node_count = 0;
+    pizza_count = 0;
     head_ptr = NULL;
 }
-Order::Order(const Order & list2bcopy)
+Order::Order(const Order &list2bcopy)
 {
-    node_count = 0;
+    pizza_count = 0;
     head_ptr = NULL;
 
     this->head_insert(list2bcopy.getHeadPtr()->data());
-    Node<Pizza>* dest_cursor = head_ptr;
-    
-    for(const Node<Pizza>* source_cursor = list2bcopy.getHeadPtr()->link(); source_cursor != NULL; source_cursor = source_cursor->link())
+    Node<Pizza> *dest_cursor = head_ptr;
+
+    for (const Node<Pizza> *source_cursor = list2bcopy.getHeadPtr()->link(); source_cursor != NULL; source_cursor = source_cursor->link())
     {
         this->list_insert(dest_cursor, source_cursor->data());
         dest_cursor = dest_cursor->link();
     }
-
 }
-Node<Pizza>* Order::getHeadPtr()
+void Order::set_phone(string phone)
+{
+    customer_phone = phone;
+}
+
+void Order::set_name(string name)
+{
+    customer_name = name;
+}
+void Order::set_cookTime(int cookTime)
+{
+    this->cook_time = cook_time;
+}
+
+string Order::get_phone(string phone) const
+{
+    return customer_phone;
+}
+string Order::get_name(string name) const
+{
+    return customer_name;
+}
+
+int Order::get_cookTime() const
+{
+    return cook_time;
+}
+
+Node<Pizza> *Order::getHeadPtr()
 {
     return head_ptr;
 }
 
-const Node<Pizza>* Order::getHeadPtr()const
+const Node<Pizza> *Order::getHeadPtr() const
 {
     return head_ptr;
 }
 
-Node<Pizza>* Order::getTailPtr()
+Node<Pizza> *Order::getTailPtr()
 {
     return tail_ptr;
 }
-const Node<Pizza>* Order::getTailPtr()const
+const Node<Pizza> *Order::getTailPtr() const
 {
     return tail_ptr;
 }
 
 size_t Order::list_length() const
 {
-    return node_count;
+    return pizza_count;
 }
 
 bool Order::isEmpty() const
@@ -54,99 +80,120 @@ bool Order::isEmpty() const
     return (head_ptr == NULL);
 }
 
-void Order::head_insert(const Node<Pizza>::value_type& value)
+void Order::head_insert(const Node<Pizza>::value_type &value)
 {
     if (head_ptr == NULL)
     {
         head_ptr = new Node<Pizza>(value);
         tail_ptr = head_ptr;
-
-    } else {
-        Node<Pizza>* temp = new Node<Pizza>(value, head_ptr);
+    }
+    else
+    {
+        Node<Pizza> *temp = new Node<Pizza>(value, head_ptr);
         head_ptr = temp;
     }
-    node_count++;
+    pizza_count++;
 }
 
-void Order::list_insert(Node<Pizza>* prev_ptr, const Node<Pizza>::value_type& value)
+void Order::list_insert(Node<Pizza> *prev_ptr, const Node<Pizza>::value_type &value)
 {
-    Node<Pizza>* temp = new Node<Pizza>(value, prev_ptr->link());
+    Node<Pizza> *temp = new Node<Pizza>(value, prev_ptr->link());
     prev_ptr->set_link(temp);
-    if(temp->link() == NULL) tail_ptr = temp;
-    node_count++;
-
+    if (temp->link() == NULL)
+        tail_ptr = temp;
+    pizza_count++;
 }
-
-
-
 
 void Order::head_remove()
 {
-    if(!isEmpty())
+    if (!isEmpty())
     {
-        Node<Pizza>* remove_ptr = head_ptr;
+        Node<Pizza> *remove_ptr = head_ptr;
         head_ptr = head_ptr->link();
         delete remove_ptr;
     }
-    if (node_count == 1) tail_ptr = head_ptr;
-    node_count--;
+    if (pizza_count == 1)
+        tail_ptr = head_ptr;
+    pizza_count--;
 }
-void Order::list_remove(Node<Pizza>* prev_ptr)
+void Order::list_remove(Node<Pizza> *prev_ptr)
 {
-    if(!isEmpty())
+    if (!isEmpty())
     {
-        
-        if(prev_ptr->link() != NULL ) 
+
+        if (prev_ptr->link() != NULL)
         {
-            Node<Pizza>* remove_ptr = prev_ptr->link();
+            Node<Pizza> *remove_ptr = prev_ptr->link();
             prev_ptr->set_link(prev_ptr->link()->link());
             delete remove_ptr;
-            if (prev_ptr == NULL) tail_ptr = prev_ptr;
-            if (node_count == 1) { tail_ptr = head_ptr; }
-            node_count--;
+            if (prev_ptr == NULL)
+                tail_ptr = prev_ptr;
+            if (pizza_count == 1)
+            {
+                tail_ptr = head_ptr;
+            }
+            pizza_count--;
         }
     }
 }
 void Order::clear_list()
 {
-    while(head_ptr != NULL)
+    while (head_ptr != NULL)
     {
         this->head_remove();
-    }   
-}
-
-void Order::operator=(const Order& source_list)
-{
-    this->head_insert(source_list.getHeadPtr()->data());
-    Node<Pizza>* dest_cursor = head_ptr;
-    
-    for(const Node<Pizza>* source_cursor = source_list.getHeadPtr()->link(); source_cursor != NULL; source_cursor = source_cursor->link())
-    {
-        this->list_insert(dest_cursor, source_cursor->data());
-        dest_cursor = dest_cursor->link();
     }
 }
 
+void Order::take_order()
+{
+    string name, phone;
+    cout << "Please enter the customer name: ";
+    cin >> name;
+    set_name(name);
+    cout << "Please enter the customer phone number: ";
+    cin >> phone;
+    set_phone(phone);
+
+    char check = 'y';
+
+    while (tolower(check) == 'y')
+    {
+        
+    }
+     
+}
+void Order::print_order()
+{
+}
+bool Order::is_ready()
+{
+}
+bool Order::is_delivered()
+{
+}
 Order::~Order()
 {
     this->clear_list();
 }
 
-void list_copy(const Order& source_list, Order& destination_list )
+void Order::add_pizza()
 {
-    if(source_list.isEmpty()) return;
+
+}
+
+void list_copy(const Order &source_list, Order &destination_list)
+{
+    if (source_list.isEmpty())
+        return;
 
     destination_list.head_insert(source_list.getHeadPtr()->data());
 
-    const Node<Pizza>* source_cursor;
-    Node<Pizza>* dest_cursor = destination_list.getHeadPtr();
+    const Node<Pizza> *source_cursor;
+    Node<Pizza> *dest_cursor = destination_list.getHeadPtr();
     for (source_cursor = source_list.getHeadPtr()->link(); source_cursor != NULL; source_cursor = source_cursor->link())
     {
-                destination_list.list_insert(dest_cursor, source_cursor->data());
-                dest_cursor = dest_cursor->link();
+        destination_list.list_insert(dest_cursor, source_cursor->data());
+        dest_cursor = dest_cursor->link();
     }
     dest_cursor->set_link(NULL);
 }
-
-
-
