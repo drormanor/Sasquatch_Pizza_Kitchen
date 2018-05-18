6f69e53d6c59833ba7052bb052e934a1ef60d607
Jordan Laidig <jrlaidig@gmail.com>
2018-05-18 09:26:38 -0700

    Updated main
    this is the main that I used

--- Code/2B Final Project/main.cpp
+++ Code/2B Final Project/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-//#include "Linked_List.h"
+#include "Linked_List.h"
 //#include "./include/Linked_List.h"
 #include "Order.h"
 #include<cstdlib>
@@ -7,14 +7,31 @@
 int main()
 {
 
-   Order o1;
-     cout << "\norder1\n\n";
+   Linked_List order_list;
+   Order myOrder;
+   string decision, exit;
+   do
+   {
+       cout << "Would you like to create an order or see the existing orders? (Enter c if you want to make an order or s if you want to see the orders.)";
+       getline(cin, decision);
 
-   o1.take_order();
-    
-   
-  
+       if(decision == "c")
+       {
+           order_list.create_order();
+       }
+       else
+       {
+           order_list.delivery_check();
+           order_list.print_list();
+       }
 
-    return EXIT_SUCCESS; 
+       cout << "Do you want to continue? (Enter either Yes or No)";
+       cin >> exit;
+       cin.ignore();
+
+
+   }while(exit == "yes");
+   return EXIT_SUCCESS; 
+   //return 0;
 
 }
